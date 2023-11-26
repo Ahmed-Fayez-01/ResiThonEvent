545703c60c2f2c097db39efd3e70a3261e981414
Fayez <ahmed.drengg@gmail.com>
2023-11-26 17:27:17 +0000

    puplish 7

--- lib/features/orginizer/home/presentation/views/organizer_home_view.dart
+++ lib/features/orginizer/home/presentation/views/organizer_home_view.dart
@@ -7,22 +7,27 @@ class OrganizerHomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(0.0), // here the desired height
-          child: AppBar(
-            elevation: 0,
-            systemOverlayStyle:  const SystemUiOverlayStyle(
-              statusBarColor: Colors.white, // <-- SEE HERE
-              statusBarIconBrightness: Brightness.dark, //<-- For Android SEE HERE (dark icons)
-              systemNavigationBarColor:Colors.white,
-              statusBarBrightness: Brightness.light, //<-- For iOS SEE HERE (dark icons)
-            ),
-          )
+    return  WillPopScope(
+      onWillPop: ()async{
+        return false;
+      },
+      child: Scaffold(
+        appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(0.0), // here the desired height
+            child: AppBar(
+              elevation: 0,
+              systemOverlayStyle:  const SystemUiOverlayStyle(
+                statusBarColor: Colors.white, // <-- SEE HERE
+                statusBarIconBrightness: Brightness.dark, //<-- For Android SEE HERE (dark icons)
+                systemNavigationBarColor:Colors.white,
+                statusBarBrightness: Brightness.light, //<-- For iOS SEE HERE (dark icons)
+              ),
+            )
+        ),
+        resizeToAvoidBottomInset: false,
+        extendBody: true,
+        body: const OrganizerHomeViewBody(),
       ),
-      resizeToAvoidBottomInset: false,
-      extendBody: true,
-      body: const OrganizerHomeViewBody(),
     );
   }
 }
--- lib/features/user/home/presentation/views/user_home_view.dart
+++ lib/features/user/home/presentation/views/user_home_view.dart
@@ -8,24 +8,29 @@ class UserHomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(0.0), // here the desired height
-          child: AppBar(
-            elevation: 0,
-            systemOverlayStyle:  const SystemUiOverlayStyle(
-              statusBarColor: Colors.white, // <-- SEE HERE
-              statusBarIconBrightness: Brightness.dark, //<-- For Android SEE HERE (dark icons)
-              systemNavigationBarColor:Colors.white,
-              statusBarBrightness: Brightness.light, //<-- For iOS SEE HERE (dark icons)
-            ),
-          )
-      ),
+    return  WillPopScope(
+      onWillPop: ()async{
+        return false;
+      },
+      child: Scaffold(
+        appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(0.0), // here the desired height
+            child: AppBar(
+              elevation: 0,
+              systemOverlayStyle:  const SystemUiOverlayStyle(
+                statusBarColor: Colors.white, // <-- SEE HERE
+                statusBarIconBrightness: Brightness.dark, //<-- For Android SEE HERE (dark icons)
+                systemNavigationBarColor:Colors.white,
+                statusBarBrightness: Brightness.light, //<-- For iOS SEE HERE (dark icons)
+              ),
+            )
+        ),
 
-      resizeToAvoidBottomInset: false,
-      extendBody: true,
-      body: const UserHomeViewBody(),
-      drawer: const UserSideMenu(),
+        resizeToAvoidBottomInset: false,
+        extendBody: true,
+        body: const UserHomeViewBody(),
+        drawer: const UserSideMenu(),
+      ),
     );
   }
 }
